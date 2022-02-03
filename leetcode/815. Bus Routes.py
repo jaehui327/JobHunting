2b2cd9157edef08010a63c6291c97de5500b6643
jaehui327 <jaeheekim970327@gmail.com>
2022-02-03 17:00:17 +0900

    🚧 815. Bus Routes

---
+++ leetcode/815. Bus Routes.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+
+        return -1
+
+sol = Solution()
+print(sol.numBusesToDestination([[1,2,7],[3,6,7]], 1, 6))
