8700c33c1d813f4ab4ed7e17c18039f6b060ada2
jaehui327 <jaeheekim970327@gmail.com>
2022-02-28 14:35:31 +0900

    🔧 convertTo1

--- elice/자료구조의 정석/convertTo1.py
+++ elice/자료구조의 정석/convertTo1.py
@@ -4,7 +4,7 @@ def convertTo1(num):
         count[i] = count[i - 1] + 1
         if i % 3 == 0:
             count[i] = min(count[i], count[i // 3] + 1)
-        elif i % 2 == 0:
+        if i % 2 == 0:
             count[i] = min(count[i], count[i // 2] + 1)
     return count[num]
 
